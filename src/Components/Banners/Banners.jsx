26fbaed96801f3d1679091be759723e2dd835f3f
Ruben Arrieta <alfredoarrieta99@gmail.com>
2023-10-09 22:45:16 -0300

    agregue en header el carrito y el buscador

--- src/Components/Banners/Banners.jsx
+++ src/Components/Banners/Banners.jsx
@@ -3,7 +3,7 @@ import "./Banners.css";
 export const Banners = () => {
     return (
         <div>
-            <img src={require('./imgBanners/banner4.png')} alt="Arma tu Pc" />
+            <img className="banners" src={require('./imgBanners/banner4.png')} alt="Arma tu Pc" />
         </div>
     )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,8 @@ export const Header = () => {
     return (
         <header className="encabezado">
             <img className="logo" src={require('./imgHeader/LogoTechCBA.png')} alt="Tech CBA logo" />
+            <input className="buscador" type="text" placeholder="Buscar..."/>
+            <img className="carrito" src={require('./imgHeader/carrito.png')} alt="Carrito" />0
         </header>
     )
 }
